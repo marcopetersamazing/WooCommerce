d855fca1d8cae845b32591a49cb069045cd90ef6
Ronald Schonewille <RSchonewille@office.multisafepay.com>
2017-04-24 15:14:11 +0200

    PLGWOO-134 nav-menus not working when plugin is active

--- WooCommerce/wp-content/plugins/multisafepay/includes/classes/MultiSafepay/Gateway/Einvoice.php
+++ WooCommerce/wp-content/plugins/multisafepay/includes/classes/MultiSafepay/Gateway/Einvoice.php
@@ -125,7 +125,7 @@ class MultiSafepay_Gateway_Einvoice extends MultiSafepay_Gateway_Abstract
         if ( !empty($settings['maxamount']) && $woocommerce->cart->total > $settings['maxamount'])
             unset($gateways['multisafepay_einvoice']);
 
-        if ($woocommerce->customer->get_country() != 'NL')
+        if (isset ($woocommerce->customer) && $woocommerce->customer->get_country() != 'NL')
             unset($gateways['multisafepay_einvoice']);
         
         return $gateways;
